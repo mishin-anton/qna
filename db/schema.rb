5fc3bbd445c2788c42d8899677cab4eba4219b2c
Anton Mishin <coal.msk@gmail.com>
2020-09-05 22:18:27 +0300

    3_task_v1

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_164122) do
+ActiveRecord::Schema.define(version: 2020_09_05_191405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
